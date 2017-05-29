fe13ce86a8bcb7776877cacbdc15d7b141569322
Obi Uchenna David <daviduchenna@outlook.com>
2017-05-30 00:52:49 +0100

    allowed hosts

--- isAStudent/settings.py
+++ isAStudent/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '6zbs*slkuaiw^x51$zhar6(qo496^$c-_tr5!^rxeb2q@#21(!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
